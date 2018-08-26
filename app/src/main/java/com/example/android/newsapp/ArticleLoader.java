29cc486f0195391f2c17fed4f03f4cc44dd0454b
talyag <talyagarman@gmail.com>
2018-08-26 13:56:16 -0700

    Adding loader

---
+++ app/src/main/java/com/example/android/newsapp/ArticleLoader.java
@@ -0,0 +1,39 @@
+package com.example.android.newsapp;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArticleLoader extends AsyncTaskLoader<List<Article>>{
+
+    public static final String LOG_TAG = ArticleLoader.class.getName();
+
+    private String mUrl;
+
+    public ArticleLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+
+    }
+
+    @Override
+    protected void onStartLoading() {
+        Log.i(LOG_TAG, "onStartLoading() has been called");
+        forceLoad();
+    }
+
+    @Override
+    public List<Article> loadInBackground() {
+        Log.i(LOG_TAG, "Test: loadInBackground(). Background thread active");
+        if (mUrl == null) {
+            return null;
+        }
+
+        List<Article> articles = QueryUtils.fetchArticleData(mUrl);
+        return articles;
+    }
+
+}
